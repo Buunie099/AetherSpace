03fb9ba58e0a9b41706d5b99acbfd68ffdc1151d
Ben Dake <124413509+Buunie099@users.noreply.github.com>
2026-05-05 16:21:39 -0400

    one sec

--- Game.cs
+++ Game.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using AetherArk
+
+namespace AetherSpace
+{
+    public class Game
+    {
+        public static void Main()
+        {
+            WindowSystem window = new WindowSystem();
+
+            var application = new Application();
+            application.Run(window);
+        }
+    }
+}
+
